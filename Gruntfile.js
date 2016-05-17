32d902763267602899a58d7f1936edc93f3e9c3a
Henry Zeitler <hzeitler@menadwork.com>
2016-05-17 14:35:06 +0200

    Set up configs

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,177 +29,228 @@
 // 'test/spec/**/*.js'
 
 module.exports = function(grunt) {
-  'use strict';
+	'use strict';
 
-  // require it at the top and pass in the grunt instance
-  require('time-grunt')(grunt);
+	// require it at the top and pass in the grunt instance
+	require('time-grunt')(grunt);
 
-  grunt.initConfig({
+	grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
+		// <%= site %> metadata comes from this file
+		site: grunt.file.readYAML('.assemble.yml'),
 
-    meta: {
-      banner: '<%= pkg.name %> by <%= pkg.author %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)',
-      bannerJS:
-      '/*\n' +
-      ' * <%= meta.banner %>\n' +
-      ' */'
-    },
-
-    clean: {
-      build: ["src/js-min/*", "src/css/*", "src/css-min/*"]
-    },
-
-    compass: {
-      dist: {
-        options: {
-          config: 'config.rb',
-          sourcemap: true
-        }
-      }
-    },
-
-    concat: {
-      js: {
-        options: {
-          separator: ';\n\r',
-          sourceMap: true
-        },
-        src: [
-          'src/js/app.js'
-        ],
-        dest: 'src/js/app.pkgd.js'
-      },
-      cssFonts: {
-        options: {
-          separator: '\n\r'
-        },
-        src: [
-          'src/fonts/style.css'
-        ],
-        dest: 'src/scss/_fonts.scss'
-      }
-    },
-
-    autoprefixer: {
-      options: {
-        browsers: ['last 100 version'],
-        map: true
-      },
-      multiple_files: {
-        expand: true,
-        flatten: true,
-        src: 'src/css/*.css',
-        dest: 'src/css/'
-      }
-    },
-
-    csswring: {
-      options: {
-        banner: '<%= meta.banner %>\n',
-        map: true,
-        preserveHacks: true
-      },
-      minify: {
-        expand: true,
-        flatten: true,
-        src: ['src/css/**/*.css'],
-        dest: 'src/css-min/'
-      }
-    },
-
-    uglify: {
-      js: {
-        options: {
-          banner: '<%= meta.bannerJS %>\n',
-          sourceMap: true
-        },
-        files: [{
-          expand: true,
-          cwd: 'src/js/',
-          src: 'src/**/*.js',
-          dest: 'src/js-min/'
-        }]
-      }
-    },
-
-    dalek: {
-      options: {
-        // invoke phantomjs, chrome & chrome canary ...
-        browser: ['phantomjs', 'chrome', 'ie'],
-        // generate an html & an jUnit report
-        reporter: ['html', 'junit']
-      },
-      dist: {
-        src: ['src/tests/dalek/*.js']
-      }
-    },
-    // TODO: Configure dest and src
-    assemble: {
-		  options: {
-		    assets: 'assets',
-		    plugins: ['permalinks'],
-		    partials: ['src/includes/**/*.hbs'],
-		    layout: ['src/components/layout/default.hbs'],
-		    data: ['src/data/*.{json,yml}']
-		  },
-		  site: {
-		    src: ['docs/*.hbs'],
-		    dest: './'
-		  }
+		meta: {
+			banner: '<%= pkg.name %> by <%= pkg.author %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)',
+			bannerJS:
+			'/*\n' +
+			' * <%= meta.banner %>\n' +
+			' */'
 		},
 
-    watch: {
+		clean: {
+			build: ["src/js-min/*", "src/css/*", "src/css-min/*"],
+      examples: ['<%= assemble.examples.dest %>/**']
+		},
+
+		compass: {
+			dist: {
+				options: {
+					config: 'config.rb',
+					sourcemap: true
+				}
+			}
+		},
+
+		concat: {
+			js: {
+				options: {
+					separator: ';\n\r',
+					sourceMap: true
+				},
+				src: [
+					'src/js/app.js'
+				],
+				dest: 'src/js/app.pkgd.js'
+			},
+			cssFonts: {
+				options: {
+					separator: '\n\r'
+				},
+				src: [
+					'src/fonts/style.css'
+				],
+				dest: 'src/scss/_fonts.scss'
+			}
+		},
+
+		autoprefixer: {
+			options: {
+				browsers: ['last 100 version'],
+				map: true
+			},
+			multiple_files: {
+				expand: true,
+				flatten: true,
+				src: 'src/css/*.css',
+				dest: 'src/css/'
+			}
+		},
+
+		csswring: {
+			options: {
+				banner: '<%= meta.banner %>\n',
+				map: true,
+				preserveHacks: true
+			},
+			minify: {
+				expand: true,
+				flatten: true,
+				src: ['src/css/**/*.css'],
+				dest: 'src/css-min/'
+			}
+		},
+
+		uglify: {
+			js: {
+				options: {
+					banner: '<%= meta.bannerJS %>\n',
+					sourceMap: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/js/',
+					src: 'src/**/*.js',
+					dest: 'src/js-min/'
+				}]
+			}
+		},
+
+		dalek: {
+			options: {
+				// invoke phantomjs, chrome & chrome canary ...
+				browser: ['phantomjs', 'chrome', 'ie'],
+				// generate an html & an jUnit report
+				reporter: ['html', 'junit']
+			},
+			dist: {
+				src: ['src/tests/dalek/*.js']
+			}
+		},
+
+		// Assemble config goes here...
+		assemble: {
+      options: {
+        flatten: true,
+        assets: '<%= site.assets %>',
+
+        // Metadata
+        pkg: '<%= pkg %>',
+        site: '<%= site %>',
+        data: ['<%= site.data %>/**/*.json'],
+        helpers: ['<%= site.helpers %>/*.js'],
+        plugins: '<%= site.plugins %>',
+
+        // General templates
+        partials: ['<%= site.includes %>/**/*.hbs'],
+        layouts: '<%= site.layouts %>',
+        layoutext: '<%= site.layoutext %>',
+        layout: '<%= site.layout %>',
+
+        // Pattern Lab templates
+        patterns: {
+          atoms: ['<%= site.atoms %>/**/*.hbs'],
+          molecules: ['<%= site.molecules %>/**/*.hbs'],
+          organisms: ['<%= site.organisms %>/**/*.hbs'],
+          templates: ['<%= site.templates %>/**/*.hbs'],
+        }
+      },
+
+      // 'pages' are specified in the src
+      site: {
+        src: ['<%= site.pages %>/*.hbs', 'src/*.hbs'],
+        dest: '<%= site.dest %>/'
+      },
+
+      patterns: {
+        options: {
+          permalinks: {
+            preset: 'pretty',
+            structure: ':pattern-:group',
+            patterns: [
+              {
+                pattern: /:pattern/,
+                replacement: function(src) {
+                  return this.src.split('/')[1];
+                }
+              },
+              {
+                pattern: /:group/,
+                replacement: function(src) {
+                  return this.src.split('/')[2];
+                }
+              }
+            ]
+          }
+        },
+        src: ['<%= site.patterns %>/**/*.hbs'],
+        dest: '<%= site.dest %>/components/'
+      }
+    },
+
+		watch: {
 
 			html: {
-        files: ['*.html'],
-        options: {
-          livereload: true
-      },
-        }
+				files: ['*.html'],
+				options: {
+					livereload: true
+			},
+				}
 
-      twig: {
-        files: ['*.twig'],
-        options: {
-          livereload: true
-        }
-      },
+			twig: {
+				files: ['*.twig'],
+				options: {
+					livereload: true
+				}
+			},
 
-      js: {
-        files: ['js/*.js'],
-        tasks: ['concat:js', 'uglify:js'],
-        options: {
-          livereload: true
-        }
-      },
+			js: {
+				files: ['js/*.js'],
+				tasks: ['concat:js', 'uglify:js'],
+				options: {
+					livereload: true
+				}
+			},
 
-      sass: {
-        files: ['scss/**/*.scss'],
-        tasks: ['compass:dist', 'autoprefixer', 'csswring:minify'],
-        options: {
-          livereload: true,
-          spawn : false       // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions.
-        }
-      }
+			sass: {
+				files: ['scss/**/*.scss'],
+				tasks: ['compass:dist', 'autoprefixer', 'csswring:minify'],
+				options: {
+					livereload: true,
+					spawn : false       // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions.
+				}
+			}
 
-    }
+		}
 
-  });
+	});
 
-  // load all plugins from the "package.json"-file
-  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+	// load all plugins from the "package.json"-file
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+	// Load Assemble
+  grunt.task.loadNpmTasks('assemble');
 
-  grunt.registerTask('default', ['watch', 'assemble']);
+	grunt.registerTask('default', ['watch', 'assemble']);
 
-  grunt.registerTask('clean-build', ['clean:build']);
-  grunt.registerTask('csswring' ['csswring:minify']);
-  grunt.registerTask('image-min', ['imagemin:dynamic']);
-  grunt.registerTask('test', ['dalek']);
+	grunt.registerTask('clean-build', ['clean:build']);
+	grunt.registerTask('csswring' ['csswring:minify']);
+	grunt.registerTask('image-min', ['imagemin:dynamic']);
+	grunt.registerTask('test', ['dalek']);
 
-  grunt.registerTask(
-      'build',
-      'Build this website ... yeaahhh!',
-      [ 'clean:build', 'concat:js', 'uglify:js', 'concat:cssFonts', 'compass:dist', 'autoprefixer', 'csswring:minify', 'assemble']
-  );
+	grunt.registerTask(
+			'build',
+			'Build this website ... yeaahhh!',
+			[ 'clean:build', 'concat:js', 'uglify:js', 'concat:cssFonts', 'compass:dist', 'autoprefixer', 'csswring:minify', 'assemble']
+	);
 
 };
